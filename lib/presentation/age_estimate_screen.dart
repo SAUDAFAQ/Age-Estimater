d1aefcc4d025368b706ff114168cae36dbb6b832
SAUDAFAQ <SAUD.AFAQ123@GMAIL.COM>
2024-06-24 22:59:10 +0200

    add validation.

--- lib/presentation/age_estimate_screen.dart
+++ lib/presentation/age_estimate_screen.dart
@@ -14,69 +14,78 @@ class AgeEstimateScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Age Estimator'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Text(
-              'Enter a name to get the estimated age',
-              style: Theme.of(context).textTheme.headlineSmall,
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(height: 20),
-            TextField(
-              controller: _nameController,
-              decoration: const InputDecoration(
-                labelText: 'Enter Name',
-                border: OutlineInputBorder(),
+      body: GestureDetector(
+        onTap: () {
+          FocusScope.of(context).unfocus();
+        },
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                'Enter a name to get the estimated age',
+                style: Theme.of(context).textTheme.headlineSmall,
+                textAlign: TextAlign.center,
               ),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                if (_nameController.text.isNotEmpty) {
-                  context
-                      .read<AgeEstimateBloc>()
-                      .add(GetAgeEvent(_nameController.text));
-                }
-              },
-              child: const Text('Get Age Estimate'),
-            ),
-            const SizedBox(height: 10),
-            ElevatedButton(
-              onPressed: () {
-                _nameController.clear();
-                context.read<AgeEstimateBloc>().add(ResetEvent());
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.grey,
+              const SizedBox(height: 20),
+              TextField(
+                controller: _nameController,
+                decoration: const InputDecoration(
+                  labelText: 'Enter Name',
+                  border: OutlineInputBorder(),
+                ),
               ),
-              child: const Text('Restart'),
-            ),
-            const SizedBox(height: 10),
-            BlocBuilder<AgeEstimateBloc, AgeEstimateState>(
-              builder: (context, state) {
-                if (state is AgeLoading) {
-                  return const Center(child: CircularProgressIndicator());
-                } else if (state is AgeLoaded) {
-                  return Text(
-                    'Estimated Age for ${_nameController.text} is: ${state.age}',
-                    style: TextStyle(color: Colors.green[800], fontSize: 16),
-                    textAlign: TextAlign.center,
-                  );
-                } else if (state is AgeError) {
-                  return Text(
-                    'Invalid username, failed to load age estimate\nPlease enter valid username',
-                    style: TextStyle(color: Colors.red[800]),
-                    textAlign: TextAlign.center,
-                  );
-                }
-                return Container();
-              },
-            ),
-          ],
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  if (_nameController.text.isNotEmpty) {
+                    context.read<AgeEstimateBloc>().add(GetAgeEvent(_nameController.text));
+                  } else {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(
+                        content: Text('Please enter a name.'),
+                      ),
+                    );
+                  }
+                },
+                child: const Text('Get Age Estimate'),
+              ),
+              const SizedBox(height: 10),
+              ElevatedButton(
+                onPressed: () {
+                  _nameController.clear();
+                  context.read<AgeEstimateBloc>().add(ResetEvent());
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.grey,
+                ),
+                child: const Text('Restart'),
+              ),
+              const SizedBox(height: 10),
+              BlocBuilder<AgeEstimateBloc, AgeEstimateState>(
+                builder: (context, state) {
+                  if (state is AgeLoading) {
+                    return const Center(child: CircularProgressIndicator());
+                  } else if (state is AgeLoaded) {
+                    return Text(
+                      'Estimated Age for ${_nameController.text} is: ${state.age}',
+                      style: TextStyle(color: Colors.green[800], fontSize: 16),
+                      textAlign: TextAlign.center,
+                    );
+                  } else if (state is AgeError) {
+                    return Text(
+                      'Invalid username, failed to load age estimate\nPlease enter valid username',
+                      style: TextStyle(color: Colors.red[800]),
+                      textAlign: TextAlign.center,
+                    );
+                  }
+                  return Container();
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
